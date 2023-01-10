e5a66a76c14b10b1391cd748ea8cda7944d558eb
Sergey Kievskiy <knhel7@mail.ru>
2023-01-10 16:47:36 +0300

    tokens was edited

--- tokens.py
+++ tokens.py
@@ -6,11 +6,11 @@ class Token:
 
     try:
         config.read('settings.ini')
-        VK = config['VKontakte']['token']
+        VK = config['VKontakte']['vk_token']
         VK_VERSION = '5.131'
-        YA = config['Yandex']['token']
+        YA = config['Yandex']['ya_token']
     except Exception as err:
-        print(err)
+        print('Отсутствует токен', err)
         VK = ''
         VK_VERSION = ''
         YA = ''
@@ -18,13 +18,15 @@ class Token:
     # print(VK, VK_VERSION, YA)  # ----------------------------------
 
     if not (VK and YA):
+        config['DEFAULT'] = {'vk_token': '', 'ya_token': ''}
+
         while not VK:
             VK = input('\nОтсутствует токен api VKontakte! Введите сервис-токен VK: ')
-        config['VKontakte'] = {'token': VK}
+        config['VKontakte'] = {'vk_token': VK}
 
         while not YA:
             YA = input('\nОтсутствует токен api Yandex! Введите свой токен Yandex: ')
-        config['Yandex'] = {'token': YA}
+        config['Yandex'] = {'ya_token': YA}
 
         with open('settings.ini', 'w') as configfile:
             config.write(configfile)
