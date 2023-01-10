0fffacee8a76e70bcefcc05e9696845a3ff60bec
Sergey Kievskiy <knhel7@mail.ru>
2023-01-10 16:19:51 +0300

    tokens was edited

--- tokens.py
+++ tokens.py
@@ -1,31 +1,31 @@
-#
-
 import configparser
 
 
 class Token:
-    configurator = configparser.ConfigParser()
+    config = configparser.ConfigParser()
 
     try:
-        configurator.read('settings.ini')
-        VK = configurator['VKontakte']['token']
+        config.read('settings.ini')
+        VK = config['VKontakte']['token']
         VK_VERSION = '5.131'
-        YA = configurator['Yandex']['token']
+        YA = config['Yandex']['token']
     except Exception as err:
         print(err)
         VK = ''
         VK_VERSION = ''
         YA = ''
 
+    print(VK, VK_VERSION, YA)
+
     if not (VK and YA):
         while not VK:
             VK = input('\nОтсутствует токен api VKontakte! Введите сервис-токен VK: ')
-        configurator['VK'] = {'token': VK}
+        config['VKontakte'] = {'token': VK}
 
         while not YA:
             YA = input('\nОтсутствует токен api Yandex! Введите свой токен Yandex: ')
-        configurator['YA'] = {'token': YA}
+        config['Yandex'] = {'token': YA}
 
-        with open('settings.ini', 'w') as config:
-            configurator.write(config)
+        with open('settings.ini', 'w') as configfile:
+            config.write(configfile)
 
